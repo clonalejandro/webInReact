16f6a602aa156937d5af9054fc082e34df8977af
clonalejandro <contact@clonalejandro.me>
2018-02-19 13:59:32 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
                             <Card cardTitle="About me" cardBody={[
                                 <p>Hi my name is <b class="bold">Alex</b> or <b class="bold">Alejandro</b>. Also known as <b class="bold">clonalejandro</b>.</p>,
                                 <p>I&apos;m to <b class="bold">Back-end</b> developer for example: PHP, Js (JavaScript), Go, Python, Ruby...<br/>Also I&apos;m to a <b class="bold">Java</b> & <b class="bold">C++</b> Developer, because I love programming in this languages.</p>,
-                                <p>Although I&apos;m <b class="bold">Spanish</b> <i class="es flag"/>person, this is a explanation about my low level in sentences in <b class="bold">English</b></p>,
+                                <p>Although I&apos;m <b class="bold">Spanish</b> <i class="es flag"/>, this is a explanation about my low level in sentences in <b class="bold">English</b></p>,
                                 <p>I work at <b class="bold">Minley</b> currently you can see my progress there: In <a href="https://github.com/MinleyNetwork/" target="_blank"><b class="bold">GitHub</b></a> or their <a href="https://www.minley.es" target="_blank"><b class="bold">Web</b></a>.<br/>On the other hand also I work in <b class="bold">DiveBlocks</b> also I leave my job for you in the <a href="https://github.com/DiveBlocks" target="_blank"><b class="bold">GitHub</b></a> is a <b class="bold">BuildTeam</b> of <b class="bold">Minecraft</b>.<br/>You can see the rest of my projects clicking <a href="./portfolio"><b class="bold">here</b></a>.</p>
                             ]}/>
                             <NewsLetter/>
